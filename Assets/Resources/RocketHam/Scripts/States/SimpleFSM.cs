5ff899d72134342115bbf800e6b4b92caaf21f29
Steven Lawson <Slawson91gd@gmail.com>
2020-09-03 15:28:42 -0500

    Latest version of Core-Player branch.
    -Updating meta files and adding temporary Finite State Machine template script

---
+++ Assets/Resources/RocketHam/Scripts/States/SimpleFSM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFSM : MonoBehaviour
+{
+    public Dictionary<EnemyStates, Action> dic;
+
+    public enum EnemyStates
+    {
+        Idle,
+        Walk
+    }
+
+    public EnemyStates EnemyState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dic = new Dictionary<EnemyStates, Action>
+        {
+            {EnemyStates.Idle, HandleIdle }
+        };
+
+        EnemyState = EnemyStates.Idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dic[EnemyState].Invoke();
+    }
+
+    private void HandleIdle()
+    {
+        // Idle code here
+        Debug.Log("Currently in the idle state.");
+    }
+}
