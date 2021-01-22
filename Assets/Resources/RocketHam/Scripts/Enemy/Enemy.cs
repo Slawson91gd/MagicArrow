6bc8e3240f198c36da747dd26d54a580873ab76d
MaurerM07 <MaurerM07@gmail.com>
2021-01-22 13:03:55 -0500

    Enemy work
    Enemy work

---
+++ Assets/Resources/RocketHam/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{ 
+ 
+    public float maxHealth;
+    public float curHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AddjustCurrentHealth(0);
+    }
+
+    public void AddjustCurrentHealth(float Damage)
+    {
+        curHealth += Damage;
+
+        if (curHealth < 0)
+            curHealth = 0;
+
+        if (maxHealth < 1)
+            maxHealth = 1;
+
+    }
+
+    
+}
